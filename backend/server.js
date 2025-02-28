740bbeb36de8ad525495fd60d10fb3a7b98e2814
anyuka007 <anna.popova.de@gmail.com>
2025-02-28 19:58:54 +0100

    feat: configure server to use environment variable for port number

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 app.use("/shoppinglist", shoppingListRouter);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
