c496568d2bbcd7752c81841da82ebcdc12390f34
anyuka007 <anna.popova.de@gmail.com>
2025-02-28 22:07:18 +0100

    feat: update CORS configuration to allow multiple origins and enable credentials; clean up Home.css

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        //origin: "http://localhost:5173",
+        origin: ["https://ap-shoppinglist.onrender.com", "http://localhost:5173"], 
+        credentials: true,
     })
 );
 
