63d417c11a039de01dceab6a510a39057bcbc985
anyuka007 <anna.popova.de@gmail.com>
2025-03-05 19:02:38 +0100

    const

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
 const isProd = import.meta.env.PROD;
 
-export const API_URL = isProd ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
+export const API_URL = isProd ? "" : import.meta.env.VITE_API_URL_DEV;
